f843f0ec24b62c6e7d66c22ec2673da8d10cc95b
movavi_school <grankina@nprofi.ru>
2022-11-01 12:05:21 +0700

    1 ноября

---
+++ individual_2/main.py
@@ -0,0 +1,28 @@
+# word= "Пограммирование"
+# # длина eng= lengthprint(len(spisok))
+# print(len(word)) # len() - команда для узнавания длины
+#
+# spisok = ["Элемент1", "Элемент2", "Элумент3"]
+
+
+phrase = "я люблю Маму"
+# МЕТОДЫ
+# #.method() - метод(модификатор)
+# lst = phrase.split(" ") # .split() - расколоть фразу по разделителю
+# print(lst)
+#
+# # П - верхний РЕГИСТР; п - нижний РЕГИСТР
+# # верхний регистр (eng) = upper case
+#  # нижний регистр (eng) = lower case
+#
+# print(phrase.upper()) # .upper() - метод для верхнего регистра
+# print(phrase.lower()) # .lower() - метод для нижнего регистра
+# print(phrase.capitalize()) # .capitalize() - метод: 1 буква большая, остальные маленькие
+
+
+# .count() - метод для подсчета количества вхождений
+phrase = "АБРАКАДАБРА"
+print(phrase.count("А"))
+print(phrase.count("AБ"))
+
+
